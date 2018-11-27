76f4f47bce975402c8d8b1231b68463b1d077b61
Oleo <oleometal@gmail.com>
2018-11-27 13:15:25 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
 res.writeHead(200, {'Content-Type': 'text/plain'});
 res.end('Hello World\n');
-}).listen(9000, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:9000/');
+}).listen(9000);
+console.log('Server running at http://190.107.155.184:9000/');
